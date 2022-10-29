3a896ad9be2697b1af1b30cacb704bd2b395afd7
carloadamos <johncarloadamos@gmail.com>
2022-10-29 13:24:19 +0800

    Periods

---
+++ src/components/Timeline.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const periods = ["Today", "This week", "This month"] as const;
+type Period = typeof periods[number];
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+        >{{ period }}</a
+      >
+    </span>
+  </nav>
+</template>

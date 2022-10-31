50490a29dedb27f8d96c129b5c2b4fa2f5777e33
carloadamos <johncarloadamos@gmail.com>
2022-10-31 10:21:19 +0800

    Filter

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,16 +1,34 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { Post, today, thisWeek, thisMonth } from "../posts";
 import { DateTime } from "luxon";
 
 const periods = ["Today", "This week", "This month"] as const;
 type Period = typeof periods[number];
 const selectedPeriod = ref<Period>("Today");
-const posts = [today, thisWeek, thisMonth].map((post) => {
-  return {
-    ...post,
-    created: DateTime.fromISO(post.created),
-  };
+const posts = computed(() => {
+  return [today, thisWeek, thisMonth]
+    .map((post) => {
+      return {
+        ...post,
+        created: DateTime.fromISO(post.created),
+      };
+    })
+    .filter((post) => {
+      switch (selectedPeriod.value) {
+        case "Today":
+          return post.created >= DateTime.now().minus({ day: 1 });
+
+        case "This week":
+          return post.created >= DateTime.now().minus({ week: 1 });
+
+        case "This month":
+          return post.created >= DateTime.now().minus({ week: 4 });
+
+        default:
+          break;
+      }
+    });
 });
 
 function selectPeriod(period: Period) {
@@ -20,6 +38,7 @@ function selectPeriod(period: Period) {
 
 <template>
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
       <a
         v-for="period of periods"
